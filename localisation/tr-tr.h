127af170df52fcdd4a1bd612b91f8a9bdcffa928
rounk-ctrl <70931017+rounk-ctrl@users.noreply.github.com>
2020-10-25 11:03:57 +0400

    Add files via upload

---
+++ localisation/tr-tr.h
@@ -0,0 +1,5 @@
+;  // Idk why it needs this but if I dont add it it will error.
+char welcome_tr[] = "NiceOs'a hos geldiniz!\n";
+char typehelplist_tr[] = "Komutlari ogrenmek icin HELP yazin.\n>";
+char helpmessage_tr[] = "Bu versiyondaki butun komutlar.\n/*-----------------------------------------*/\nCLS          : Ekrani temizler\nPAGE         : Page bilgisini gosterir!\nHALT         : CPU'yu durdurur.\nABOUT        : Isletim sistemi hakkinda bilgi verir.\n"
+// Add more stuff here.
