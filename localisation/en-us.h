570722ec0a5fdb381cf73a2060613a5ae3a70e95
rounk-ctrl <70931017+rounk-ctrl@users.noreply.github.com>
2021-02-22 09:25:37 +0400

    Add files via upload

---
+++ localisation/en-us.h
@@ -0,0 +1,4 @@
+char welcome_en[] = "Welcome to NiceOS!\n";
+char typehelplist_en[] = "Type HELP for a list of commands.\n> ";
+char helpmessage_en[] = "All commands in this OS as of this version!\n/*-----------------------------------------*/\nCLS          : Clears the screen!\nPAGE         : Shows Page info. For debugging purpose only!\nHALT         : Halts the CPU!\nABOUT        : Shows information about the OS\n"
+// Add more stuff here. (cant as of now)
